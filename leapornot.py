e194bc397acfd8141ff8dfa12054a3253991445e
Sudhan1996 <sudharsan1996@gmail.com>
2016-07-28 18:21:29 +0930

    Add files via upload

---
+++ leapornot.py
@@ -0,0 +1,20 @@
+#-------------------------------------------------------------------------------
+# Name:        module1
+# Purpose:
+#
+# Author:      Student
+#
+# Created:     28/07/2016
+# Copyright:   (c) Student 2016
+# Licence:     <your licence>
+#-------------------------------------------------------------------------------
+
+def main():
+ num = input("enter the number")
+ if(num%4==0):
+  print("The Given Year is a leap year")
+ else:
+  print("The given year is not a leap year")
+
+if __name__ == '__main__':
+    main()
